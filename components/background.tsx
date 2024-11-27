047c60089dbc78a5363cfd3df7d9668c33c104f7
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2024-11-27 16:19:21 +0100

    Remove @vercel/blob

--- components/background.tsx
+++ components/background.tsx
@@ -1,45 +1,17 @@
-import { Suspense } from "react";
-import { list } from "@vercel/blob";
-import Image from "next/image";
-
-import poster from "../public/poster.png";
-
-async function Video({ prefix }: { prefix: string }) {
-  const { blobs } = await list({ prefix, limit: 1 });
-  const { url } = blobs[0];
-
+export default function Background() {
   return (
     <video
       className="fixed inset-0 w-full h-full object-cover"
-      preload="none"
       autoPlay
-      muted
-      loop
-      playsInline
       controls={false}
+      loop
+      muted
+      playsInline
+      poster="/IMG_7170.webp"
+      preload="none"
     >
-      <source src={url} type="video/mp4" />
+      <source src="/IMG_7170.webm" type="video/webm" />
     </video>
   );
 }
 
-export default function Background() {
-  return (
-    <Suspense
-      fallback={
-        <Image
-          className="fixed inset-0 w-full h-full object-cover"
-          src={poster}
-          alt="Poster"
-          width={1214}
-          height={2160}
-          placeholder="blur"
-          sizes="100vw"
-          priority
-        />
-      }
-    >
-      <Video prefix="IMG_7170-nOqHfkiTopRM3JVs55jWyvku7qVuno.mp4" />
-    </Suspense>
-  );
-}
